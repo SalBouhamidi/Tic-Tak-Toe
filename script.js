fa14feae48c5034b8911e19f0b5ed8bd902b462c
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-08-30 15:29:04 +0100

    writing O and x

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function boardmaker(id){
     board = document.createElement("div");
     board.setAttribute("class", `horizantal${id}`);
-    board.setAttribute("onClick", `detectPositionCol(${id})`)
+    // board.setAttribute("onClick", `detectPositionCol(${id})`)
     document.querySelector(".main_section_bordgame").appendChild(board);
     for(let j= 0; j < 20; j++){
         divcreator(id, j);
@@ -12,22 +12,38 @@ function boardmaker(id){
 function divcreator (id, j){
     cases = document.createElement("div");
     cases.setAttribute("class", "main_section_bordgame_case")
-    cases.setAttribute("id", `main_section_bordgame_case+${id}`);
-    cases.setAttribute("onClick", `detectPositionRow(${j})`)
+    cases.setAttribute("id", `main_section_bordgame_case${id}-${j}`);
+    cases.setAttribute("onClick", `position(${j}, ${id})`)
     document.querySelector(`.horizantal${id}`).appendChild(cases);
+
 }
 
-function detectPositionRow(j){
-    console.log(`row`+ j);
-    localStorage.setItem('row', j);
-    console.log(localStorage.getItem('row'));
-    
+
+
+function position(j,id){
+    console.log('the col pos '+ id);
+    console.log('the row pos' + j);
+    const click = document.getElementById(`main_section_bordgame_case${id}-${j}`);
+
+    if(click.innerHTML === '' ){
+        if(localStorage.getItem('lastchar') === 'X'){
+            click.innerHTML = 'O'
+            localStorage.setItem('lastchar', 'O');
+            // console.log(localStorage.getItem('lastchar'));
+        }else if(localStorage.getItem('lastchar') === 'O')
+        {
+            click.innerHTML = 'X'
+            localStorage.setItem('lastchar', 'X');
+            // console.log(localStorage.getItem('lastchar'));
+        }
+    }else{
+        alert('choose an empty case');
+    }
+
 }
-function detectPositionCol(id){
-    alert('col'+id)
-    // localStorage.setItem('col', id)
-    // console.log(localStorage.getItem('col'));
-}
+
+
+
 
 
 

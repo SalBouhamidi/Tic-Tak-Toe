35ac63cf06f6db0bb86efbe5acd52efdbf58e3cd
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-08-30 17:27:08 +0100

    detectwinner not finished yet

--- script.js
+++ script.js
@@ -19,29 +19,58 @@ function divcreator (id, j){
 }
 
 
-
 function position(j,id){
-    console.log('the col pos '+ id);
-    console.log('the row pos' + j);
+    detectwinner();
     const click = document.getElementById(`main_section_bordgame_case${id}-${j}`);
 
     if(click.innerHTML === '' ){
         if(localStorage.getItem('lastchar') === 'X'){
             click.innerHTML = 'O'
             localStorage.setItem('lastchar', 'O');
+            whoGannaPlay()
             // console.log(localStorage.getItem('lastchar'));
         }else if(localStorage.getItem('lastchar') === 'O')
         {
             click.innerHTML = 'X'
             localStorage.setItem('lastchar', 'X');
+            whoGannaPlay()
             // console.log(localStorage.getItem('lastchar'));
         }
     }else{
         alert('choose an empty case');
     }
+    // console.log(localStorage.getItem('lastchar'));
+    // console.log(click.id)
 
 }
+function whoGannaPlay(){
+    player = document.createElement("p");
+    player.setAttribute('class', 'player');
+    document.querySelector('.main_section_role_determinator').appendChild(player);
+    // player.innerHTML= 'ok'
+    let result = localStorage.getItem('lastchar');
+    if(result == 'X'){
+        document.querySelector('.player').innerHTML = 'O: Ganna play';
+    }else if(result == 'O'){
+        document.querySelector('.player').innerHTML = 'X: ganna play'
+    }
+    // console.log(player.innerHTML)
+}
 
+function detectwinner(){
+
+    let prefix = 'main_section_bordgame_case19-';
+    let max= 20;
+    for(let j =0; `${prefix}${j}`< `${prefix}${max}`; j++){
+        for(let i = 0; document.getElementById(`${prefix}${i}`).id < `${prefix}${j}` ; i++){
+            // console.log('heygg');
+        }
+    }
+    console.log(document.getElementById(`${prefix}19`).id)
+
+    
+ 
+}
 
 
 
@@ -55,6 +84,7 @@ function boardgame (){
     for(let i = 0; i <size; i ++){
         boardmaker(i);
     }
+    whoGannaPlay()
 }
 boardgame();
 

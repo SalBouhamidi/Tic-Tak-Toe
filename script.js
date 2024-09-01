b51063c77fbbeaa0fd5a74f97dff6fe64f5dcfff
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-09-01 10:23:35 +0100

    The error is fixed and the Horizntal match is working yee

--- script.js
+++ script.js
@@ -3,26 +3,26 @@ function boardmaker(id){
     board = document.createElement("div");
     board.setAttribute("class", `horizantal${id}`);
     document.querySelector(".main_section_bordgame").appendChild(board);
-    for(let col= 0; col < 20; col++){
-        divcreator(id, col);
+    for(let row= 0; row < 20; row++){
+        divcreator(id, row);
     }
 }
 
-function divcreator (id, col){
+function divcreator (id, row){
     cases = document.createElement("div");
     cases.setAttribute("class", "main_section_bordgame_case")
-    cases.setAttribute("id", `${id}-${col}`);
-    cases.setAttribute("onClick", `position(${col}, ${id})`)
+    cases.setAttribute("id", `${id}-${row}`);
+    cases.setAttribute("onClick", `position(${row}, ${id})`)
     document.querySelector(`.horizantal${id}`).appendChild(cases);
-    // console.log(`rooow:${id}-cool:${col}`)
+    // console.log(`rooow:${id}-cool:${row}`)
 
 }
 
 
-function position(col,id){
+function position(row,id){
 
-    const click = document.getElementById(`${id}-${col}`);
-
+    const click = document.getElementById(`${id}-${row}`);
+ 
     if(click.innerHTML === '' ){
         if(localStorage.getItem('lastchar') === 'X'){
             click.innerHTML = 'O'
@@ -37,11 +37,9 @@ function position(col,id){
             // console.log(localStorage.getItem('lastchar'));
         }
     }else{
-        alert('choose an empty case');
+       click.style.cursor ='Not-allowed';
     }
-    const myTimeout = setTimeout(detectwinner, 2000);
-
-
+    detectwinner(click,id , row);
 
 }
 function whoGannaPlay(){
@@ -58,33 +56,116 @@ function whoGannaPlay(){
 }
 
 
-function detectwinner(){
-    let count= 0;
-    for(let row = 0; row <20; row++){
-        for(let col= 0; col <20; col++){
-            const caseid = document.getElementById(`${col}-${row}`).innerText;
-            if(col< 19){
-                const caseidplusOne = document.getElementById(`${col+1}-${row}`).innerText;
-                if(caseid !== ''){
-                    if(caseidplusOne){
-                        if(caseid == caseidplusOne){
-                            count= count+1;
-                        }else{
-                            count = 0;
-                        }
-                    }
-    
-                }
-
-            }
-
- 
-
-        }
-        console.log('counter counting' + count);
-        return count;
+function stepForward(number){
+    if(number <= 15){
+        step= 4;
+        return step;
+    }else if(number > 15 && number <= 19){
+        step = 19-number;
+        return step;
     } 
 }
+
+function stepback(number){
+    if(number >= 4){
+        step= 4;
+        return step;
+    }else if(number < 4){
+        step = number;
+        return step;
+    }
+}
+
+function detectwinner(click, id, row) {
+let countX = 1;
+let countO = 1;
+let step = stepForward(id);
+let total = id+step;
+let stepb = stepback(id);
+// console.log(stepb);
+let totalBack = id-stepb;
+// console.log(totalBack);
+
+for(let col = id; col < total; col++){
+    if(col < 19){
+        let cell= document.getElementById(`${col}-${row}`).innerText;
+        let cellpluspone = document.getElementById(`${col+1}-${row}`).innerHTML;
+            if(cell == cellpluspone){
+                if(cell == 'X'){
+                    countX++;
+                    // console.log( countX);
+                    if(countX == 5){
+                        alert('the X is the winner');
+                        break;
+                    }else{
+                        for(let col = id; col > totalBack; col--){
+                            if(col > 0){
+                                let cell= document.getElementById(`${col}-${row}`).innerText;
+                                let cellpluspone = document.getElementById(`${col-1}-${row}`).innerHTML;
+                                if(cell == cellpluspone){
+                                    if(cell == 'X'){
+                                        countX++;
+                                        // console.log('wlah ila bseh ')
+                                        if(countX == 5){
+                                            console.log('heet')
+                                            alert('the X is winner');
+                                            break;
+                                        }
+
+                                    }
+                                }
+                                
+
+
+                            }
+                        }
+
+                    }
+                }else if(cell == 'O'){
+                    console.log("ooooooo");
+                    countO++;
+                    if(countO == 5){
+                        alert('the O is the winner');
+                        break;
+                    }else{
+                        for(let col = id; col > totalBack; col--){
+                            if(col > 0){
+                                let cell= document.getElementById(`${col}-${row}`).innerText;
+                                let cellpluspone = document.getElementById(`${col-1}-${row}`).innerHTML;
+                                if(cell == cellpluspone){
+                                    if(cell == 'O'){
+                                        countO++;
+                                        // console.log('counter o is workng')
+                                        if(countO == 5){
+                                            alert('the X is winner');
+                                            break;
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+    else{                    
+            console.log("zeeeeeeeeeero");
+            countX = 0;
+            countO = 0;
+        }
+
+    }else{
+        break;
+    }
+}
+
+console.log('countx' +countX);
+console.log('county' +countO);
+
+
+}
+
 
 
 function boardgame (){
